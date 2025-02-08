8e5d6712ee248a9a0110e5d2317e3222a483983f
marder <marcin.derucki@gmail.com>
2025-02-08 16:22:12 +0100

    08.02.2025

---
+++ 01_plotting.m
@@ -0,0 +1,7 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*ResourceFunction["DarkMode"][]*)
+
+
+Graphics3D[{Opacity[.8], Glow[RGBColor[1,0,0]], EdgeForm[White], Lighting -> None, PolyhedronData["Echidnahedron", "Faces"]}]
